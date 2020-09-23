eccb570306c3b482fcac845876f5425aed57a162
Mark Edney <68298107+mark-edney@users.noreply.github.com>
2020-09-23 13:01:58 -0400

    Corrected assignment

--- run_analysis.R
+++ run_analysis.R
@@ -14,8 +14,10 @@ subject_train<-read.table("UCI HAR Dataset\\train\\subject_train.txt")
 subject_test<-read.table("UCI HAR Dataset\\test\\subject_test.txt")
 
 
-#Initial merge of two datasets
-datamerge<-merge(train_x,test_x,all=TRUE)
+#Initial merge of two datasets and ensurees no missing data
+data2<-merge(train_x,test_x,all=TRUE)
+data2<-data2[complete.cases(data2),]
+
 
 #Store the variable labels
 featurelist<-read.table("UCI HAR Dataset\\features.txt")
@@ -26,20 +28,23 @@ activity<-rbind(train_y,test_y)
 
 #Sorts the dataset for mean and std in the variable names
 sortfeatures<-grepl(".*[Mm][Ee][Aa][Nn].*|.*[Ss][Tt][Dd].*",featurelist[,2])
-datamerge<-datamerge[,sortfeatures]
+data2<-data2[,sortfeatures]
 
 #Labels the dataset with correct variables based on the filter
-names(datamerge)<-featurelist[sortfeatures,2]
+names(data2)<-featurelist[sortfeatures,2]
 
 #adds the activity data to the main dataset and changes the stored data to the descriptive activity name
-datamerge<-mutate(datamerge,"activity"=activity)
-datamerge$activity<-activitylabel[unlist(datamerge$activity),2]
+data2<-mutate(data2,"activity"=activity)
+data2$activity<-activitylabel[unlist(data2$activity),2]
 
 #adds the subject data
-datamerge<-mutate(datamerge,"subject"=rbind(subject_train,subject_test))
+data2<-mutate(data2,"subject"=rbind(subject_train,subject_test))
 
 #creates a second dataframe that is grouped by activity and then subject
-datasecond<-group_by(datamerge,activity, subject)
+datasecond<-group_by(data2,activity, subject)
 
 #summarizes that dataframe with the mean value
-datasecond<-summarise_all(datasecond,mean)
+datasecond<-summarise_at(datasecond,c(1:86),mean,na.rm = TRUE)
+
+#writes the second df into a file
+write.table(datasecond,"tidydata.txt",row.names = FALSE)
